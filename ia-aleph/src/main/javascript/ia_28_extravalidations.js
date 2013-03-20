b2127e19fb9bf03cf2f85ffc11b4247dc263bb53
Davide Imbriaco <davide.imbriaco@gmail.com>
2013-03-20 21:07:02 +0100

    [no commit message]

--- ia-aleph/src/main/javascript/ia_28_extravalidations.js
+++ ia-aleph/src/main/javascript/ia_28_extravalidations.js
@@ -122,6 +122,7 @@
 	
         if(units.sectorialName==='Morat Aggression Force'){
             units.unitsByIsc['Yaogat Strike Infantry'].childsByCode['Lieutenant'].swc='0';
+            units.unitsByIsc['Kornak Gazarot'].childsByCode['Lt'].swc='0';
         }
         if(units.sectorialName==='Shavastii Expeditionary Force'){
             units.unitsByIsc['Shasvastii Aswuangs'].childsByCode['Lieutenant'].swc='0';
