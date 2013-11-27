6243e2128e9bfad31b7ea0b4dca58d8bbdb7facb
Davide Imbriaco <davide.imbriaco@gmail.com>
2013-11-27 14:39:24 +0100

    .

--- ia-aleph_android_2/src/it/anyplace/alephtoolbox2/ArmyListController.java
+++ ia-aleph_android_2/src/it/anyplace/alephtoolbox2/ArmyListController.java
@@ -2,18 +2,21 @@ package it.anyplace.alephtoolbox2;
 
 import it.anyplace.alephtoolbox2.beans.Events;
 import it.anyplace.alephtoolbox2.services.ArmyListService;
-import it.anyplace.alephtoolbox2.services.ArmyListService.ArmyList.ArmyListUnit;
 import it.anyplace.alephtoolbox2.services.CurrentListService;
 import it.anyplace.alephtoolbox2.services.CurrentListService.UnitRecord;
+import it.anyplace.alephtoolbox2.services.DataService.UnitData;
 
 import java.util.List;
 
 import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import com.google.common.base.Function;
-import com.google.common.collect.Lists;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
@@ -52,16 +55,40 @@ public class ArmyListController {
 	private void loadArmylist() {
 //		reset();
 		List<UnitRecord> unitRecords = currentListService.getUnitRecords();
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
-				android.R.layout.simple_list_item_1, Lists.transform(
-						unitRecords,
-						new Function<UnitRecord, String>() {
+//		ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity,
+//				android.R.layout.simple_list_item_1, Lists.transform(
+//						unitRecords,
+//						new Function<UnitRecord, String>() {
+//
+//							@Override
+//							public String apply(UnitRecord model) {
+//								return model.getIsc();
+//							}
+//						}));
+		ArrayAdapter<UnitRecord> adapter = new ArrayAdapter<UnitRecord>(activity,
+				R.layout.armylist_record, unitRecords) {
 
-							@Override
-							public String apply(UnitRecord model) {
-								return model.getIsc();
-							}
-						}));
+			@Override
+			public View getView(int position, View convertView, ViewGroup parent) {
+				if (convertView == null) {
+					convertView = LayoutInflater.from(activity).inflate(
+							R.layout.armylist_record, null);
+				}
+				UnitRecord unitRecord = getItem(position);
+				((TextView) convertView.findViewById(R.id.armyListRecordIsc))
+				.setText(unitRecord.getIsc());
+				((TextView) convertView.findViewById(R.id.armyListRecordCode))
+						.setText(unitRecord.getCode());
+
+				((ImageView) convertView.findViewById(R.id.armyListRecordIcon))
+						.setImageResource(activity.getResources()
+								.getIdentifier(
+										"unitlogo_" + unitRecord.getUnitData().getCleanIsc(),
+										"drawable", activity.getPackageName()));
+				return convertView;
+			}
+
+		};
 		mainRosterList.setAdapter(adapter);
 		// for(ArmyListUnit model:armylist.getModels()){
 		// mainRosterList
--- ia-aleph_android_2/src/it/anyplace/alephtoolbox2/UnitDetailController.java
+++ ia-aleph_android_2/src/it/anyplace/alephtoolbox2/UnitDetailController.java
@@ -2,9 +2,20 @@ package it.anyplace.alephtoolbox2;
 
 import it.anyplace.alephtoolbox2.services.CurrentListService;
 import it.anyplace.alephtoolbox2.services.DataService.UnitData;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.common.base.Joiner;
 import com.google.common.eventbus.EventBus;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -20,19 +31,84 @@ public class UnitDetailController {
 	private ViewFlipperController viewFlipperController;
 	@Inject
 	private EventBus eventBus;
-	
-	private TextView unitDetailIscView;
-	
+
+	private TextView unitDetailName, unitDetailCode, unitDetailCost,
+			unitDetailSwc, unitDetailType, unitDetailMov, unitDetailCc,
+			unitDetailBs, unitDetailPh, unitDetailWip, unitDetailArm,
+			unitDetailBts, unitDetailW, unitDetailSpecs;
+	private ImageView unitDetailImage;
 
 	@Inject
 	private void init() {
-		unitDetailIscView = (TextView) activity.findViewById(R.id.unitDetailIsc);
-		
+		unitDetailName = (TextView) activity.findViewById(R.id.unitDetailName);
+		unitDetailCode = (TextView) activity.findViewById(R.id.unitDetailCode);
+		unitDetailCost = (TextView) activity.findViewById(R.id.unitDetailCost);
+		unitDetailSwc = (TextView) activity.findViewById(R.id.unitDetailSwc);
+		unitDetailType = (TextView) activity.findViewById(R.id.unitDetailType);
+		unitDetailMov = (TextView) activity.findViewById(R.id.unitDetailMov);
+		unitDetailCc = (TextView) activity.findViewById(R.id.unitDetailCc);
+		unitDetailBs = (TextView) activity.findViewById(R.id.unitDetailBs);
+		unitDetailPh = (TextView) activity.findViewById(R.id.unitDetailPh);
+		unitDetailWip = (TextView) activity.findViewById(R.id.unitDetailWip);
+		unitDetailArm = (TextView) activity.findViewById(R.id.unitDetailArm);
+		unitDetailBts = (TextView) activity.findViewById(R.id.unitDetailBts);
+		unitDetailW = (TextView) activity.findViewById(R.id.unitDetailW);
+		unitDetailSpecs = (TextView) activity
+				.findViewById(R.id.unitDetailSpecs);
+		unitDetailImage = (ImageView) activity
+				.findViewById(R.id.unitDetailImage);
+
 		eventBus.register(this);
 	}
-	
-	public void openUnitDetail(UnitData unitData){
-		unitDetailIscView.setText(unitData.getIsc());
+
+	public void loadUnitDetail(UnitData unitData) {
+		unitDetailName.setText(unitData.getName());
+		unitDetailCode.setText(unitData.getCode());
+		unitDetailCost.setText(unitData.getCost());
+		unitDetailSwc.setText(unitData.getSwc());
+		unitDetailType.setText(unitData.getType());
+		unitDetailMov.setText(unitData.getMov());
+		unitDetailCc.setText(unitData.getCc());
+		unitDetailBs.setText(unitData.getBs());
+		unitDetailPh.setText(unitData.getPh());
+		unitDetailWip.setText(unitData.getWip());
+		unitDetailArm.setText(unitData.getArm());
+		unitDetailBts.setText(unitData.getBts());
+		unitDetailW.setText(unitData.getW());
+		unitDetailSpecs.setText(Joiner.on(", ").join(unitData.getSpec()));
+
+		unitDetailImage.setImageResource(R.drawable.missing_logo_big);
+		if (!unitData.getCbcode().isEmpty()) {
+			// TODO show loading screen
+			String cbCode = unitData.getCbcode().get(0);// TODO slideshow
+			final String url = "http://www.infinitythegame.com/infinity/catalogo/"
+					+ cbCode + "-recorte.png";
+			new AsyncTask<Void, Void, Bitmap>() {
+
+				@Override
+				protected Bitmap doInBackground(Void... params) {
+					InputStream in;
+					try {
+						in = new java.net.URL(url).openStream();
+						Bitmap bitmap = BitmapFactory.decodeStream(in);
+						return bitmap;
+					} catch (MalformedURLException e) {
+						Log.w("Error loading unit icon", e);
+					} catch (IOException e) {
+						Log.w("Error loading unit icon", e);
+					}
+					return null;
+				}
+
+				protected void onPostExecute(Bitmap result) {
+					unitDetailImage.setImageBitmap(result);
+				}
+			}.execute();
+		}
+	}
+
+	public void openUnitDetail(UnitData unitData) {
+		loadUnitDetail(unitData);
 		viewFlipperController.showUnitDetailView();
 	}
 }
