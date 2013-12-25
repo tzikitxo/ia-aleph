765aaf49329a8952ceb0ad99e235954da3e34add
Davide Imbriaco <davide.imbriaco@gmail.com>
2013-12-25 23:36:18 +0100

    .

--- ia-aleph/bin/exportToAndroid.sh
+++ ia-aleph/bin/exportToAndroid.sh
@@ -15,6 +15,10 @@ echo "packaging data to $target"
 	echo ']'
 } > $target/armylist_data.json
 
+#cat ia-data_*_specs.js > $target/specs_data.js
+cat ia-data_*_weapons_data.json > $target/weapons_data.json
+cat ia-data_*_sectorials_data.json > $target/sectorials_data.json
+
 echo "copying icons"
 {
 	for file in ~/svn/ia-aleph/ia-aleph/src/main/webapp/images/*_logo.png; do
