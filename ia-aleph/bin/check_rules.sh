b39378a83efc6b8c5a4e49db6bccfc0bacbedc0f
Davide Imbriaco <davide.imbriaco@gmail.com>
2014-02-27 15:58:55 +0100

    .

--- ia-aleph/bin/check_rules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-xmllint --schema src/main/webapp/data/rules.xsd src/main/webapp/data/rules.xml --noout || exit
-xml tr src/main/webapp/data/rules.xsl src/main/webapp/data/rules.xml | grep -v '<meta' | xml val -
